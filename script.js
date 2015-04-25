7ac8b94ff68dad035b69abda7a75ce6358b1dbe6
Stevey Z <sjzerneri@gmail.com>
2015-04-25 09:34:39 -0700

    adding re-size button

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-var squares = prompt("How Many Squares Do You Want?");
-
 $(document).ready(function () {
   var $newdiv = "<div class='square'> </div>";
-  for (var i=0; i < squares; i++) {
-    $(".matrix").append($newdiv);
-  }
+  for (var i=1; i < 256; i++) {
+    $("#matrix").append($newdiv);
+  };
 
   $(".square").hover(function() {
     $(this).addClass("hover_color");
-  })
-
+  });
 });
+
+function newGrid(){
+  var newSize = prompt('Enter how many squares you want per row and column');
+
+  var newWH = 600 / newSize;
+
+  $("#matrix").empty();
+
+  for (var i=1; i < newSize * newSize; i++) {
+    $("#matrix").append("<div class='square'> </div>");
+  };
+
+  $('.square').css('width',newWH);
+  $('.square').css('height',newWH);
+}
