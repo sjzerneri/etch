d19ae57ccb39872ed0fdfb9d7e07d577f819bec1
Stevey Z <sjzerneri@gmail.com>
2015-04-25 10:24:57 -0700

    fixing border to outline

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var $newdiv = "<div class='square'> </div>";
-  for (var i=1; i < 256; i++) {
+  for (var i=0; i < 256; i++) {
     $("#matrix").append($newdiv);
   };
 
@@ -10,16 +10,23 @@ $(document).ready(function () {
 });
 
 function newGrid(){
+//ask the user what the size of the new grid should be
   var newSize = prompt('Enter how many squares you want per row and column');
 
-  var newWH = 600 / newSize;
 
+//clear the current grid from the page
   $("#matrix").empty();
 
-  for (var i=1; i < newSize * newSize; i++) {
+//append new grid based on user input
+  for (var i=0; i < (newSize * newSize); i++) {
     $("#matrix").append("<div class='square'> </div>");
   };
 
+  //log the width and height of the new squares
+      var newWH = 600 / newSize;
+
+// Set the square div width and height
   $('.square').css('width',newWH);
   $('.square').css('height',newWH);
+
 }
