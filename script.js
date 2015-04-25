7e52d17d01980caa5501761a77e861867393e39d
Stevey Z <sjzerneri@gmail.com>
2015-04-24 20:10:56 -0700

    adding hover function to etch

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var squares = prompt("How Many Rows Do You Want?");
+var squares = prompt("How Many Squares Do You Want?");
 
 $(document).ready(function () {
   var $newdiv = "<div class='square'> </div>";
   for (var i=0; i < squares; i++) {
     $(".matrix").append($newdiv);
   }
+
+  $(".square").hover(function() {
+    $(this).addClass("hover_color");
+  })
+
 });
